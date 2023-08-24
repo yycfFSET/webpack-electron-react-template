862e41f2c49af018f80511bdd02719a42351fad7
yanyunchangfeng <yanyunchangfeng@gmail.com>
2023-08-24 21:04:00 +0800

    feat: add mutationObserver

--- src/workbench/electron-renderer/index.tsx
+++ src/workbench/electron-renderer/index.tsx
@@ -1,14 +1,19 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import './index.scss';
 import { CloseOutlined, MinusOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { Button, message } from 'antd';
 import { ipcRenderer } from 'electron';
 import { useService } from 'src/base/common/injector';
 import EnvironmentService from 'src/platform/modal/environment/common/EnvironmentService';
 document.body.innerHTML = `<div id="root"></div>`;
 
+declare global {
+  interface Window {
+    __toastResult: boolean;
+  }
+}
 const environmentService = useService(EnvironmentService);
 const rendererChannel = new BroadcastChannel('renderer-channel');
 const pMsg = (msg: string = 'hello modal window') => {
@@ -26,6 +31,31 @@ const App: FC = () => {
     let res = await ipcRenderer.invoke('closeWin', 'c', 'd');
     console.log(res);
   };
+  useEffect(() => {
+    const obs = new MutationObserver((mutations) => {
+      mutations.forEach((mutationRecord) => {
+        const success = ['.ant-message-success'];
+        const { addedNodes, type } = mutationRecord;
+        if (type === 'childList' && addedNodes.length > 0) {
+          success.forEach((selector) => {
+            const els = document.querySelector(selector);
+            if (els) {
+              window.__toastResult = true;
+            }
+          });
+        }
+      });
+    });
+    obs.observe(document.body, { childList: true });
+    const id = setInterval(() => {
+      message.success('成功');
+    }, 3000);
+
+    return () => {
+      clearInterval(id);
+      obs.disconnect();
+    };
+  }, []);
   return (
     <div className="d-flex container">
       <div className="windowActionWrap">
